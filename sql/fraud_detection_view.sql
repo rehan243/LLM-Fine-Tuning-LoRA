e5a8ffcafbe2ab9c9b79dfd028d2b3511baedb0e
Rehan Malik <46004319+rehan243@users.noreply.github.com>
2026-07-24 09:27:46 -0600

    add a view for detecting potential fraud cases

--- sql/fraud_detection_view.sql
+++ sql/fraud_detection_view.sql
@@ -1,26 +1,30 @@
-create or replace view fraud_detection_view as
+create view fraud_detection as
 select 
-    transaction_id,
-    user_id,
-    amount,
+    transaction_id, 
+    user_id, 
+    transaction_amount, 
     transaction_date,
     case 
-        when amount > 1000 then 'high_value'
-        when amount between 500 and 1000 then 'medium_value'
-        else 'low_value'
-    end as transaction_value,
+        when transaction_amount > (select avg(transaction_amount) from transactions) * 2 
+            then 'high_value'
+        when transaction_amount < 10 
+            then 'low_value'
+        else 'normal_value'
+    end as transaction_category,
     case 
-        when user_id in (select user_id from flagged_users) then true
-        else false
-    end as is_flagged
+        when exists (
+            select 1 
+            from blacklisted_users 
+            where blacklisted_users.user_id = transactions.user_id
+        ) 
+            then 'blacklisted'
+        else 'not_blacklisted'
+    end as user_status
 from 
     transactions
 where 
     transaction_date >= current_date - interval '30 days'
-    and transaction_status = 'completed';
+    and transaction_status = 'completed'
+    and (user_status = 'blacklisted' or transaction_category = 'high_value')
 
--- TODO: consider adding more filters based on user behavior
--- e.g., check for unusual patterns in transaction frequency
-
--- this view will help in analyzing potentially fraudulent transactions
--- and assist in generating reports for the fraud detection team
+-- TODO: consider adding more filters or grouping options based on feedback
